178f66ff92129030dfc21eefc83c76aa682f7e5d
Hayat-123 <hayatmohammed307@gmail.com>
2025-05-13 23:15:47 -0700

    our first java code in git

---
+++ a.java
@@ -0,0 +1,17 @@
+public class Account {
+
+  private double balance;
+
+  public double rate = 2.5;
+
+  public void deposite(double amount) {
+  }
+
+  private double getbalance() {
+  return 0.0;
+  }
+
+  public void setRate(double intRate) {
+  }
+
+}
